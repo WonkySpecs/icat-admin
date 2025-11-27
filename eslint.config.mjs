7874a5156ef2545cc977d29d03db4ed452084984
Will Taylor <william.h.taylor@stfc.ac.uk>
2025-11-27 11:24:59 +0000

    Add react-hooks eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,47 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
+import reactHooks from 'eslint-plugin-react-hooks';
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {fileURLToPath} from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-export default defineConfig([globalIgnores(["**/build/"]), {
+export default defineConfig([
+  globalIgnores(["**/build/"]),
+  reactHooks.configs.flat['recommended-latest'], {
     extends: compat.extends(
-        "preact",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@tanstack/eslint-plugin-query/recommended",
+      "preact",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@tanstack/eslint-plugin-query/recommended",
     ),
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
     },
 
     rules: {
-        "prefer-template": "off",
-        radix: "off",
+      "prefer-template": "off",
+      radix: "off",
 
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-unused-vars": ["error", {
+        argsIgnorePattern: "^_",
+      }],
 
-        "react/jsx-tag-spacing": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "jest/no-deprecated-functions": "off",
+      "react/jsx-tag-spacing": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "jest/no-deprecated-functions": "off",
     },
-}]);
+  }]);
