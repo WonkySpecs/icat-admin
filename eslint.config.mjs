0a44d6d149a660c6276d5475debe9064e360f42f
Will Taylor <william.h.taylor@stfc.ac.uk>
2025-11-27 12:13:54 +0000

    Remove eslint-config-preact
    Hasn't updated to be compatible with modern ESLint/import. When that happens, should be easy to readd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,28 +2,12 @@ import {defineConfig, globalIgnores} from "eslint/config";
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-import js from "@eslint/js";
-import {FlatCompat} from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
 
 export default defineConfig([
   globalIgnores(["**/build/"]),
   reactHooks.configs.flat["recommended-latest"],
   tseslint.configs.recommended,
   {
-    extends: compat.extends(
-      "preact",
-    ),
-
     languageOptions: {
       parser: tsParser,
     },
